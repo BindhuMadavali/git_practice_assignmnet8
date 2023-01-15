fabcc8fde8502e0c99682806076f974d2b7b4a94
Bindhu Madavali <bindhumadavali2000@gmail.com>
2023-01-15 14:26:02 +0530

    code created

---
+++ prime.js
@@ -0,0 +1,14 @@
+let number =15;
+let count =0;
+for(let i=1;i<=number;i++){
+
+    if(number%i==0){
+        count++;
+    }
+}
+if(count==2){
+    console.log("prime number")
+}
+else{
+    console.log*"not prime "
+}
